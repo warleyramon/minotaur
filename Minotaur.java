6e0ee204659d15af3d90b2af89639077620ca8e5
warleyramon <161717728+warleyramon@users.noreply.github.com>
2024-03-08 09:23:05 -0300

    Version_1.2.0

--- Minotaur.java
+++ Minotaur.java
@@ -4,8 +4,11 @@ import robocode.*;
 import java.awt.Color;
 
 
-public class Minotaur extends AdvancedRobot
-{
+public class Minotaur extends AdvancedRobot{
+	boolean movingForward;
+	int dist = 50;
+	double turnGunAmt, angle;
+
 	public void run() {
 		
 		setBodyColor(Color.black);
@@ -15,23 +18,64 @@ public class Minotaur extends AdvancedRobot
 		setScanColor(Color.green);
 
 		while(true) {
-			setAhead(100);
+			setAhead(4000);
+			movingForward = true;
 			setTurnRight(10);
 			setTurnGunLeft(80);
 			execute();
+			
 		}
 	}
 	
 
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(7);
+	public void onHitWall(HitWallEvent e) {
+		reverseDirection();
 	}
 
-	public void onHitByBullet(HitByBulletEvent e) {
-		back(55);
+	public void reverseDirection() {
+		if (movingForward) {
+			setBack(4000);
+			setTurnRight(9);
+			setTurnGunLeft(80);
+			movingForward = false;
+			execute();
+		} else {
+			setAhead(4000);
+			setTurnRight(9);
+			setTurnGunLeft(80);
+			movingForward = true;
+			execute();
+		}
 	}
-	
-	public void onHitWall(HitWallEvent e) {
-		back(55);
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if (e.getDistance() < 50 && getEnergy() > 50) {
+			fire(3);
+		} 
+		else {
+			fire(7);
+		}
+		scan();
+	}
+
+
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(normalRelativeAngleDegrees(90 - (getHeading() - e.getHeading())));
+
+		ahead(dist);
+		dist *= -1;
+		scan();
+	}
+
+	public static double normalRelativeAngleDegrees(double angle) {
+		return (angle %= 360) >= 0 ? (angle < 180) ? angle : angle - 360 : (angle >= -180) ? angle : angle + 360;
+	}
+
+	public void onHitRobot(HitRobotEvent e) {
+		double turnGunAmt = normalRelativeAngleDegrees(e.getBearing() + getHeading() - getGunHeading());
+
+		turnGunRight(turnGunAmt);
+		fire(3);
 	}	
+	
 }
